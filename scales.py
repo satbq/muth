647f94941829f3c1c8e1421cc17957707356b117
satbq <120515622+satbq@users.noreply.github.com>
2024-02-25 12:41:25 -0700

    first complete working version of BrightnessGraph

--- scales.py
+++ scales.py
@@ -1,12 +1,14 @@
 from manim import *
 from set_theory import rotate_list
 from set_theory import start_zero
+import networkx as nx
 
 ROMAN_NUMERALS = ["N", "I", "II", "III", "IV", "V", "VI",
                   "VII", "VIII", "IX", "X", "XI", "XII", "XIII",
                   "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                   "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII",
                   "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV"]
+
 
 def scalar_interval_matrix(pc_set, edo=12):
     res = np.vstack([*[rotate_list(pc_set, i) for i in range(len(pc_set))]])
@@ -15,12 +17,28 @@ def scalar_interval_matrix(pc_set, edo=12):
     return res
 
 
+def modecompare(pc_set, reference, rounder=10):
+    difference = np.round(np.array(pc_set)-np.array(reference), decimals=rounder)
+    res = np.sum(np.unique(np.sign(difference)))
+    return res.astype(int)
+
+
+def brightness_comps(pc_set, edo=12, rounder=10):
+    card = len(pc_set)
+    res = np.zeros([card, card])
+    modes = scalar_interval_matrix(pc_set, edo=edo)
+    for i in range(card):
+        for j in range(card):
+            res[i, j] = modecompare(modes[:, i], modes[:, j], rounder=rounder)
+    return res
+
+
 class BrightnessGraph(VMobject):
-    def __init__(self, pc_set, position_matrix=None, adjacency_matrix=None,
-                 edo=12,
-                 pc_scale=.5, pc_color=BLACK,
-                 rn_scale=1, rn_color=BLACK,
-                 sum_scale=.8, sum_color=BLACK,
+    def __init__(self, pc_set, position_matrix=None,
+                 edo=12, arrow_stroke_width=3, arrow_tip_length=.2,
+                 pc_scale=.35, pc_color=BLACK, v_buff=0.8, h_buff=.7,
+                 rn_scale=.5, rn_color=BLACK,
+                 sum_scale=.4, sum_color=BLACK,
                  **kwargs):
         VMobject.__init__(self, **kwargs)
 
@@ -29,7 +47,11 @@ class BrightnessGraph(VMobject):
         SIM_row_sums = current_SIM.sum(axis=0)
 
         mode_labels = VGroup(*[Tex(ROMAN_NUMERALS[i]).set_color(rn_color).scale(rn_scale) for i in range(1, card+1)])
-        mode_pcs = Matrix(current_SIM.transpose()).set_color(pc_color).scale(pc_scale)
+
+        mode_pcs = Matrix(current_SIM.transpose(), v_buff=v_buff, h_buff=h_buff).set_color(pc_color).scale(pc_scale)
+        for bracket in mode_pcs.get_brackets():
+            mode_pcs.remove(bracket)
+
         mode_sums = VGroup(*[Tex("(", mode_sum, ")").set_color(sum_color).scale(sum_scale) for mode_sum in SIM_row_sums])
         nodes = VGroup()
 
@@ -37,9 +59,37 @@ class BrightnessGraph(VMobject):
             new_node = VGroup(mode_labels[i], mode_sums[i], mode_pcs.get_rows()[i])
             new_node[1].next_to(new_node[0], RIGHT)
             new_node[0].align_to(new_node[1], UP)
-            new_node[2].next_to(new_node[0], DOWN).match_x(VGroup(new_node[0], new_node[1]))
+            new_node[2].next_to(new_node[0], DOWN, buff=SMALL_BUFF).match_x(VGroup(new_node[0], new_node[1]))
             nodes.add(new_node)
 
-        self.add(mode_labels)
-        self.add(mode_pcs)
+        unreduced_matrix = brightness_comps(pc_set=pc_set, edo=edo)
+        unreduced_matrix[unreduced_matrix > 0] = 0
+        unreduced_graph = nx.from_numpy_array(unreduced_matrix, create_using=nx.DiGraph)
+        reduced_graph = nx.transitive_reduction(unreduced_graph)
+        reduced_matrix = nx.adjacency_matrix(reduced_graph)
+        reduced_matrix = reduced_matrix.todense()
+
+        nodes.arrange_in_grid(cols=2, buff=LARGE_BUFF)
+
+        for row, node in zip(position_matrix, nodes):
+            node.move_to(row)
+
+        arrows = VGroup()
+        for i in range(card):
+            for j in range(card):
+                if reduced_matrix[i, j] > 0:
+                    arrows.add(Arrow(nodes[i].get_top(),
+                                     nodes[j].get_bottom(),
+                                     stroke_width=arrow_stroke_width, tip_length=arrow_tip_length,
+                                     max_stroke_width_to_length_ratio=10,
+                                     max_tip_length_to_length_ratio=.5,
+                                     buff=SMALL_BUFF).set_color(BLACK))
+
+        # self.add(mode_labels)
+        # self.add(mode_pcs)
+        # self.add(mode_sums)
         self.node = nodes
+        self.arrow = arrows
+        self.add(nodes)
+        self.add(arrows)
+        # self.add(arrows)
